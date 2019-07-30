0e6662d43dbd7f37203048eb69d45d6e9d816785
jinalshah999 <jinalshah999@gmail.com>
2019-07-30 16:03:35 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { CalcComponent } from './calc/calc.component';
-import { ArrdemoComponent } from './arrdemo/arrdemo.component';
-import { ApphighDirective } from './apphigh.directive';
-import { HighlighterDirective } from './highlighter.directive';
-import { CardDirective } from './card.directive';
-import { FilterPipe } from './filter.pipe';
-
+import { AppComponent } from "./app.component";
+import { DemoComponent } from "./demo/demo.component";
+import { CalcComponent } from "./calc/calc.component";
+import { ArrdemoComponent } from "./arrdemo/arrdemo.component";
+import { ApphighDirective } from "./apphigh.directive";
+import { HighlighterDirective } from "./highlighter.directive";
+import { CardDirective } from "./card.directive";
+import { FilterPipe } from "./filter.pipe";
+import { MenuComponent } from "./menu/menu.component";
+import { routing } from "./app.routing";
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,13 +20,11 @@ import { FilterPipe } from './filter.pipe';
     ApphighDirective,
     HighlighterDirective,
     CardDirective,
-    FilterPipe
+    FilterPipe,
+    MenuComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule, routing],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from "@angular/router";
+import { DemoComponent } from "./demo/demo.component";
+import { ArrdemoComponent } from "./arrdemo/arrdemo.component";
+import { CalcComponent } from "./calc/calc.component";
+
+const arr: Routes = [
+  { path: "demo", component: DemoComponent },
+  { path: "arrdemo", component: ArrdemoComponent },
+  { path: "calc", component: CalcComponent }
+];
+
+export const routing = RouterModule.forRoot(arr);
